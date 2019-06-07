3a2b0eca7fb76f522fee4fd44bfbffd0dd561bb5
Марчук Алексей <MarchukAlex@AREON.LOCAL>
2019-06-07 15:44:16 +0300

    Add project files.

---
+++ CandidateTest.Threads/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
+
+namespace CandidateTest.Threads
+{
+    class Program
+    {
+        static CancellationTokenSource cts = new CancellationTokenSource();
+        static DateTime timeToBeCompleted;
+        static bool completedByTime;
+        static System.Timers.Timer aTimer;
+        static void Main(string[] args)
+        {
+            aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 100;
+            aTimer.Enabled = true;
+
+            timeToBeCompleted = DateTime.Now.AddMinutes(1);
+            Console.WriteLine("Should be automatically stopped at " + timeToBeCompleted.ToShortTimeString());
+            Process currentProcess = Process.GetCurrentProcess();
+
+            Console.WriteLine("Press ESCAPE to stop the process.");
+            Console.WriteLine(string.Format("RAM used: {0} MB", currentProcess.WorkingSet64 / 1024 / 1024));
+
+            for (int i = 1; i <= 200; i++)
+            {
+                WorkerProcess p = new WorkerProcess("P#" + i.ToString(), 200 + i * 2, cts);
+                p.Start();
+            }
+
+            while (Console.ReadKey(true).Key == ConsoleKey.Escape && !completedByTime)
+            {
+                Process currentProcessExit = Process.GetCurrentProcess();
+                Console.WriteLine("----------------------Terminating Process ESCAPE...");
+                Console.WriteLine(string.Format("\r\nRAM used: {0} MB", currentProcessExit.WorkingSet64 / 1024 / 1024));
+
+                cts.Cancel();
+            }
+            Console.WriteLine("Press ANY KEY");
+            Console.ReadKey();
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            if (timeToBeCompleted <= DateTime.Now)
+            {
+                Process currentProcessExit = Process.GetCurrentProcess();
+                Console.WriteLine("----------------------Terminating Process by time...");
+                Console.WriteLine(string.Format("\r\nRAM used: {0} MB", currentProcessExit.WorkingSet64 / 1024 / 1024));
+                cts.Cancel();
+                aTimer.Enabled = false;
+                completedByTime = true;
+            }
+        }
+    }
+}
---
+++ CandidateTest.Threads/WorkerProcess.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace CandidateTest.Threads
+{
+    public class WorkerProcess
+    {
+        private static ReaderWriterLockSlim _readWriteLock = new ReaderWriterLockSlim();
+        private bool isCompleted;
+        private int _cnt = 0;
+        private static string _statistics;
+        private CancellationTokenSource _cts;
+        private delegate void RetryHandler(string message);
+        private event RetryHandler OnRetry;
+        private byte[] _passedData;
+        private static List<KeyValuePair<string, string>> _data { get; set; }
+        public static List<KeyValuePair<string, string>> Data
+        {
+            get
+            {
+                if (_data == null)
+                {
+                    _data = new List<KeyValuePair<string, string>>();
+                }
+                return _data;
+            }
+            set
+            {
+                _data = value;
+            }
+        }
+
+        public string ProcessName { get; }
+        public int TimeOut { get; }
+
+        public WorkerProcess(string processName, int timeOut, CancellationTokenSource cts)
+        {
+            ProcessName = string.IsNullOrWhiteSpace(processName) ? "Noname Process" : processName;
+            TimeOut = timeOut > 0 ? timeOut : 500;
+            _cts = cts;
+            _statistics = string.Empty;
+            _cnt = 0;
+        }
+
+        public void Start()
+        {
+            var mainThread = new Thread(() =>
+            {
+                while (!isCompleted)
+                {
+                    _cnt++;
+                    _cts.Token.Register(() =>
+                    {
+                        isCompleted = true;
+                    });
+                    try
+                    {
+                        _readWriteLock.EnterWriteLock();
+                        var fs = File.Open("..\\..\\Output\\data.txt", FileMode.Append);
+                        Data.Add(new KeyValuePair<string, string>(ProcessName, DateTime.UtcNow.ToString() + " \t TimeOut : " + TimeOut.ToString() + " \t\t " + ProcessName + "(" + _cnt + ")" + Environment.NewLine));
+                        _passedData = new UTF8Encoding(true).GetBytes(Data.LastOrDefault(x => x.Key == ProcessName).Value);
+                        byte[] bytes = _passedData;
+                        fs.Write(bytes, 0, bytes.Length);
+                    }
+                    catch (Exception ex)
+                    {
+                        OnRetry += Retry;
+                        OnRetry(ex.Message);
+                    }
+                    finally
+                    {
+                        _readWriteLock.ExitWriteLock();
+                    }
+                    Thread.Sleep(TimeOut);
+                    var statisticsThread = new Thread(() =>
+                    {
+                        SaveStatistics();
+                    });
+                    statisticsThread.Start();
+                }
+            });
+
+            mainThread.Start();
+        }
+
+        private static void Retry(string message)
+        {
+            Console.WriteLine(message);
+            string error = message.Trim();
+            Data.Add(new KeyValuePair<string, string>("Error", error));
+        }
+
+        public static void SaveStatistics()
+        {
+            try
+            {
+                var fs = File.Open("..\\..\\Output\\Statistics.txt", FileMode.Open);
+                var stat = Data.GroupBy(x => x.Key).OrderBy(x => x.Key);
+                _statistics = String.Format("{0,10} | {1,10}", "Process", "Count") + Environment.NewLine;
+                _statistics += new String('-', 24) + Environment.NewLine;
+                foreach (var process in stat)
+                {
+                    _statistics += String.Format("{0,10} | {1,10}", process.Key, process.Count()) + Environment.NewLine;
+                }
+
+                var toSave = new UTF8Encoding(true).GetBytes(_statistics);
+                fs.Write(toSave, 0, toSave.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
