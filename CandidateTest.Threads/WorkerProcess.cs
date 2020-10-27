739105b9d7479f48daaf2cbe538ca4b247c463b6
VitalyiGribinik <gvitaha@outlook.com>
2020-10-27 16:08:34 +0200

    Fix threads and exceptions

--- CandidateTest.Threads/WorkerProcess.cs
+++ CandidateTest.Threads/WorkerProcess.cs
@@ -9,16 +9,19 @@ namespace CandidateTest.Threads
 {
     public class WorkerProcess
     {
-        private static ReaderWriterLockSlim _readWriteLock = new ReaderWriterLockSlim();
+        // private static ReaderWriterLockSlim _readWriteLock = new ReaderWriterLockSlim();
         private bool isCompleted;
         private int _cnt = 0;
-        private static string _statistics;
+        private string _statistics;
         private CancellationTokenSource _cts;
         private delegate void RetryHandler(string message);
         private event RetryHandler OnRetry;
         private byte[] _passedData;
-        private static List<KeyValuePair<string, string>> _data { get; set; }
-        public static List<KeyValuePair<string, string>> Data
+        private List<KeyValuePair<string, string>> _data { get; set; }
+
+        private static object _Lock = new object();
+        private string separator = Path.DirectorySeparatorChar.ToString();
+        public List<KeyValuePair<string, string>> Data
         {
             get
             {
@@ -57,62 +60,74 @@ namespace CandidateTest.Threads
                     {
                         isCompleted = true;
                     });
-                    try
+                    lock (_Lock)
                     {
-                        _readWriteLock.EnterWriteLock();
-                        var fs = File.Open("..\\..\\Output\\data.txt", FileMode.Append);
-                        Data.Add(new KeyValuePair<string, string>(ProcessName, DateTime.UtcNow.ToString() + " \t TimeOut : " + TimeOut.ToString() + " \t\t " + ProcessName + "(" + _cnt + ")" + Environment.NewLine));
-                        _passedData = new UTF8Encoding(true).GetBytes(Data.LastOrDefault(x => x.Key == ProcessName).Value);
-                        byte[] bytes = _passedData;
-                        fs.Write(bytes, 0, bytes.Length);
+                        try
+                        {
+                            using (var fs = File.Open($"..{separator}..{separator}Output{separator}data.txt", FileMode.Append))
+                            {
+                                Data.Add(new KeyValuePair<string, string>(ProcessName, DateTime.UtcNow.ToString() + " \t TimeOut : " + TimeOut.ToString() + " \t\t " + ProcessName + "(" + _cnt + ")" + Environment.NewLine));
+                                _passedData = new UTF8Encoding(true).GetBytes(Data.LastOrDefault(x => x.Key == ProcessName).Value);
+                                byte[] bytes = _passedData;
+                                fs.Write(bytes, 0, bytes.Length);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            OnRetry += Retry;
+                            OnRetry(ex.Message);
+                        }
+                        Thread.Sleep(TimeOut);
+
                     }
-                    catch (Exception ex)
-                    {
-                        OnRetry += Retry;
-                        OnRetry(ex.Message);
-                    }
-                    finally
-                    {
-                        _readWriteLock.ExitWriteLock();
-                    }
-                    Thread.Sleep(TimeOut);
-                    var statisticsThread = new Thread(() =>
-                    {
-                        SaveStatistics();
-                    });
-                    statisticsThread.Start();
+                    SaveStatistics();
+
                 }
+
             });
+
+            //var statisticsThread = new Thread(() =>
+            //{
+            //    SaveStatistics();
+            //});
+
+
+            //statisticsThread.Start();
 
             mainThread.Start();
         }
 
-        private static void Retry(string message)
+        private void Retry(string message)
         {
             Console.WriteLine(message);
             string error = message.Trim();
             Data.Add(new KeyValuePair<string, string>("Error", error));
         }
 
-        public static void SaveStatistics()
+        public void SaveStatistics()
         {
-            try
+            lock (_Lock)
             {
-                var fs = File.Open("..\\..\\Output\\Statistics.txt", FileMode.Open);
-                var stat = Data.GroupBy(x => x.Key).OrderBy(x => x.Key);
-                _statistics = String.Format("{0,10} | {1,10}", "Process", "Count") + Environment.NewLine;
-                _statistics += new String('-', 24) + Environment.NewLine;
-                foreach (var process in stat)
+                try
                 {
-                    _statistics += String.Format("{0,10} | {1,10}", process.Key, process.Count()) + Environment.NewLine;
-                }
+                    using (var fs = File.Open($"..{separator}..{separator}Output{separator}Statistics.txt", FileMode.Open))
+                    {
+                        var stat = Data.GroupBy(x => x.Key).OrderBy(x => x.Key);
+                        _statistics = String.Format("{0,10} | {1,10}", "Process", "Count") + Environment.NewLine;
+                        _statistics += new String('-', 24) + Environment.NewLine;
+                        foreach (var process in stat)
+                        {
+                            _statistics += String.Format("{0,10} | {1,10}", process.Key, process.Count()) + Environment.NewLine;
+                        }
 
-                var toSave = new UTF8Encoding(true).GetBytes(_statistics);
-                fs.Write(toSave, 0, toSave.Length);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
+                        var toSave = new UTF8Encoding(true).GetBytes(_statistics);
+                        fs.Write(toSave, 0, toSave.Length);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
         }
     }
